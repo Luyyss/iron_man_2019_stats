c3551f20c73c693b4a3e7223b4306794f14e4bec
Luis <luisdig91@gmail.com>
2020-09-29 08:41:59 -0300

    Inclusão de libs no requirements.txt

--- src/pages/home.py
+++ src/pages/home.py
@@ -53,92 +53,6 @@ def write():
     atletas_f.reset_index(drop=True)
     st.dataframe(atletas_f.head(10))
 
-    
-    # df = pd.DataFrame({
-    #     'label': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-    #     'value1': [1, 3, 2, 4, 5, 6, 4],
-    #     'value2': [2, 1, 2, 5, 3, 4, 2],
-    # })
-
-    # alt.Chart(df).transform_fold(
-        # ['value1', 'value2'],
-        # as_=['column', 'value']
-    # ).mark_bar().encode(
-        # x='label:N',
-        # y='value:Q',
-        # color='column:N'
-    # )
-
-    # df = pd.DataFrame({
-    #     'label': atletas_m['Name'],
-    #     'swim': atletas_m['Swim'],
-    #     'T1': atletas_m['T1'],
-    #     'bike': atletas_m['Bike'],
-    #     'T2': atletas_m['T2'],
-    #     'run': atletas_m['Run']
-    # })
-
-    # st.dataframe(df)
-
-    # b = alt.Chart(df).transform_fold(
-    #     ['swim', 'T1', 'bike', 'T2', 'run'],
-    #     as_=['column', 'value']
-    # ).mark_bar().encode(
-    #     x='label:N',
-    #     y='value:Q',
-    #     color='column:N'
-    # )
-
-    # st.altair_chart(b)
-
-
-
-
-    ##
-    ##  Bar Charts
-    ##
-    ## {"yield": 33.06666, "variety": "No. 475", "year": 1931, "site": "Duluth"},
-
-    # bars = alt.Chart(arT).mark_bar().encode(
-    #     x=alt.X('bike:Q', stack='zero'),
-    #     y=alt.Y('name:N'),
-    #     color=alt.Color('bike')
-    # )
-
-    # text = alt.Chart(arT).mark_text(dx=-15, dy=3, color='white').encode(
-    #     x=alt.X('bike:Q', stack='zero'),
-    #     y=alt.Y('name:N'),
-    #     detail='site:N',
-    #     text=alt.Text('sum(yield):Q', format='.1f')
-    # )
-
-    # st.altair_chart(bars)
-
-
-    # df1=pd.DataFrame(10*np.random.rand(6,1),index=["A","B","C","D", "E", "F"],columns=["N"])
-    # df2=pd.DataFrame(10*np.random.rand(6,1),index=["A","B","C","D", "E", "F"],columns=["N"])
-    # df3=pd.DataFrame(10*np.random.rand(6,1),index=["A","B","C","D", "E", "F"],columns=["N"])
-
-    # df1 = prep_df(df1, 'DF1')
-    # df2 = prep_df(df2, 'DF2')
-    # df3 = prep_df(df3, 'DF3')
-
-    # df = pd.concat([df1, df2, df3])
-
-    # st.dataframe(df)
-
-    # b = alt.Chart(df).mark_bar().encode(
-    #     x=alt.X('c2:N', title=None),
-    #     y=alt.Y('sum(values):Q', axis=alt.Axis(grid=False, title=None)),
-    #     column=alt.Column('c1:N', title=None),
-    #     color=alt.Color('DF:N', scale=alt.Scale(range=['#96ceb4', '#ffcc5c','#ff6f69']))
-    # ).configure_view(
-    #     strokeOpacity=0    
-    # )
-
-    # st.altair_chart(b)
-
-
     atletas_m = atletas_m.head(10)
 
     atletas_m['SwimN'] = atletas_m['Swim'].apply(funcs.strToSeconds)
@@ -147,17 +61,11 @@ def write():
     atletas_m['T1N'] = atletas_m['T1'].apply(funcs.strToSeconds)
     atletas_m['T2N'] = atletas_m['T2'].apply(funcs.strToSeconds)
 
-    # st.dataframe(atletas_m)
-
     df1=pd.DataFrame(atletas_m['SwimN'].tolist(), index=atletas_m['Name'], columns=["N"])
     df2=pd.DataFrame(atletas_m['T1N'].tolist(), index=atletas_m['Name'], columns=["N"])
     df3=pd.DataFrame(atletas_m['BikeN'].tolist(), index=atletas_m['Name'], columns=["N"])
     df4=pd.DataFrame(atletas_m['T2N'].tolist(), index=atletas_m['Name'], columns=["N"])
     df5=pd.DataFrame(atletas_m['RunN'].tolist(), index=atletas_m['Name'], columns=["N"])
-
-    # st.dataframe(df1)
-    # st.dataframe(df2)
-    # st.dataframe(df3)
 
     df1 = prep_df(df1, 'Swim')
     df2 = prep_df(df2, 'T1')
@@ -168,12 +76,12 @@ def write():
     df = pd.concat([df1, df2, df3, df4, df5])
 
     b = alt.Chart(df).mark_bar().encode(
-        x=alt.X('c1:N', title=None),
-        y=alt.Y('sum(values):Q', axis=alt.Axis(grid=False, title=None)),
+        x=alt.Y('c1:N', title=None),
+        y=alt.X('values:Q', axis=alt.Axis(grid=False, title=None)),
         column=alt.Column('c2:N', title=None),
         color=alt.Color('Classe:N', scale=alt.Scale(range=['#96ceb4', '#ffcc5c','#ff6f69']))
     ).configure_view(
-        strokeOpacity=0    
+        strokeOpacity=0
     )
 
     st.altair_chart(b)
